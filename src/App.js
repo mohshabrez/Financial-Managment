b99cc56336b270e36e741709f7061f993225062f
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2023-12-19 13:34:47 +0530

    Update Main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { NavBar } from './Components/NavBar';
+import { Incomes } from './Pages/Incomes';
+import { Expenses } from './Pages/Expenses';
+import { Savings } from './Pages/Savings';
+import { Reports } from './Pages/Reports';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gray-800 min-h-screen">
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Incomes/>}/>
+        <Route path="/expenses" element={<Expenses/>}/>
+        <Route path="/savings" element={<Savings/>}/>
+        <Route path='/reports' element={<Reports/>}/>
+      </Routes>
     </div>
   );
 }
